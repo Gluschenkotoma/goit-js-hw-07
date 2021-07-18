c5507af27e8b9ee314a5d9af683ac85abe40863d
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-07-18 12:25:29 +0300

    3

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// const categoriesEl = document.querySelector("#categories");
+// const itemEl = document.querySelector(".item");
+// const titleEl = document.querySelector("h2");
+
+// console.log(categoriesEl);
+// console.log(itemEl);
+// console.log(titleEl);
+
+const refs = {
+  categoriesList: document.querySelector("#categories"),
+  itemList: document.querySelector(".item"),
+  title: document.querySelector("h2"),
+};

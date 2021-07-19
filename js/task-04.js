0cd51ab9669dab3d9e26120b1b987518fc3f07b1
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-07-19 14:15:20 +0300

    25

---
+++ js/task-04.js
@@ -0,0 +1,9 @@
+const decrementBtnEl = document.querySelector('[data-action="decrement"]');
+const incrementBtnEl = document.querySelector('[data-action="increment"]');
+const counterEl = document.querySelector("#value");
+
+// let counterValue = 0;
+// const decrement = () => (value.textContent = counterValue -= 1);
+// decrementBtnEl.addEventListener("click", decrement);
+// const increment = () => (value.textContent = counterValue += 1);
+// incrementBtnEl.addEventListener("click", increment);

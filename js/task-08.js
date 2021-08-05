145436bf95bf6d155e884bc1eab88b98ab9a997d
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-08-05 15:13:00 +0300

    28

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,24 @@
-const boxesContainer = document.querySelector("#boxes");
-const input = document.querySelector('[type="number"]');
-const renderBtn = document.querySelector('[data-action="render"]');
-const clearBtn = document.querySelector('[data-action="destroy"]');
+const boxesContainer = document.querySelector("#boxes"); //cсылка на div id="boxes"
+const input = document.querySelector('[type="number"]'); //cсылка на input type="number"
+console.log(input);
+const renderBtn = document.querySelector('[data-action="render"]'); //Создать отрисовать
+const clearBtn = document.querySelector('[data-action="destroy"]'); //Очистить
 
 renderBtn.addEventListener("click", getAmount);
 clearBtn.addEventListener("click", destroyBoxes);
 
 function getAmount() {
-  const amount = Number(input.value);
+  const amount = Number(input.value); //к-во input приведенное к числу
+
   createBoxes(amount);
 }
 
 function createBoxes(amount) {
   let basicSize = 30;
-  const fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment(); //ссылка на пустой обьект (фрагмент)
   for (let i = 0; i < amount; i += 1) {
     let size = basicSize + i * 10;
-    const div = document.createElement("div");
+    const div = document.createElement("div"); //создание бокса со свойствами
     div.style.width = size + "px";
     div.style.height = size + "px";
     div.style.margin = "10px";
@@ -27,10 +29,10 @@ function createBoxes(amount) {
 }
 
 function destroyBoxes() {
-  boxesContainer.innerHTML = "";
+  boxesContainer.innerHTML = ""; //Заменяет содержимое тела пустой строкой
   input.value = "";
 }
 
 function random() {
-  return Math.floor(Math.random() * 256);
+  return Math.floor(Math.random() * 256); //подбор рандомных цветов
 }

f1ee8a3c21cba45eccfa187812e46831e98b7f60
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-08-05 19:08:02 +0300

    29

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,8 @@
 const listEl = document.querySelectorAll(".item");
 const allItems = listEl.forEach((value) =>
   console.log(
-    `Категория - ${value.firstElementChild.textContent}, количество элементов -  ${value.lastElementChild.children.length}`
+    `Категория - ${value.firstElementChild.textContent}, количество элементов -
+      ${value.lastElementChild.children.length}`
   )
 );
 // console.log(value) - все элементы в listEl
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,13 @@
 const input = document.querySelector("#validation-input");
 const inputValueLength = Number(input.getAttribute("data-length"));
+console.log(inputValueLength); //6
 
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
+  console.log(event.currentTarget.value); //если ввести в инпут 1,2,3 -> консоль 123
+  console.log(event.currentTarget.length); //если ввести в инпут 1,2,3 -> консоль длинна ввода символов 3
+
   if (event.currentTarget.value.length === inputValueLength) {
     event.currentTarget.classList.add("valid");
     event.currentTarget.classList.remove("invalid");
